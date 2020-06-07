7224b5eef5bb878317e5e9d212e5801545bcee8a
Alexander Watts <acwatts.dev@gmail.com>
2020-06-07 20:16:21 +0100

    Made api use cors so we can make calls from other ports

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const PORT = 4000;
 
 const app = express();
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('API app');
+  res.send('api connected');
 });
 
 app.listen(PORT, () => console.log(`API app running on port ${PORT}`));
