265f8920efb80e13e670b0ec5b4f1b626757568c
Alexander Watts <acwatts.dev@gmail.com>
2020-06-07 20:17:52 +0100

    Made call to api fetching the data sent on home route

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
+import axios from 'axios';
 
-const App = () => <div>Frontend app</div>;
+const App = () => {
+  const [data, setData] = useState(null);
+  axios.get('http://localhost:4000/')
+    .then(({ data }) => setData(data))
+    .catch(() => setData('api not loaded'))
+  return (
+    <div>Frontend app: {data}</div>
+  )
+}
 
 ReactDom.render(<App />, document.getElementById("root"));
